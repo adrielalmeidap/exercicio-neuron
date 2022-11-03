14df49491b2ad8b84150e9b3f254465cd3592f88
adriel <adrielalmeidap@gmail.com>
2022-11-03 07:13:12 -0300

    first commit. created the structure of front and back

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          <div className="title">Activity Neuron</div>
+          <div className="subtitle">Create a CRUD with React and Spring Boot</div>
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
